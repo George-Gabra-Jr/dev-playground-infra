9578a870e1d7e408a929effd7ef8133a1d81731f
George Gabra <george.gabra@progressiosolutions.com>
2024-09-14 21:52:04 +0300

    adjusted docker entrypoint

--- pipelines/entrypoint.sh
+++ pipelines/entrypoint.sh
@@ -4,4 +4,4 @@
 buildkitd --root /home/jenkins/.local/share/buildkit --config /home/jenkins/.config/buildkit/buildkitd.toml &
 
 # Start Jenkins
-exec /sbin/tini -- /usr/local/bin/jenkins.sh
+exec /usr/bin/tini -- /usr/local/bin/jenkins.sh
